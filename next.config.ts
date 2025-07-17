521b1c5da634cf2e00d8b3962a8406ecaa8294ca
Maksim <craash00077@gmail.com>
2025-07-17 10:00:55 +0300

    апдейт конфига

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: "export",
   basePath: "/CurriculumVitae",
-  assetPrefix: "/CurriculumVitae/", 
+  assetPrefix: "/CurriculumVitae",
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
 };
-
 export default nextConfig;
